c20702a36a1d964efa78ae8c7d595e31d0396821
fyras1 <firas.ismail69@gmail.com>
2020-05-02 11:25:12 +0100

    2

--- work.R
+++ work.R
@@ -2,6 +2,8 @@ library(ggplot2)
 library(caret)
 library(dplyr)
 library(rattle)
+library(corrplot)
+library(dplyr)
 
 download.file("https://d396qusza40orc.cloudfront.net/predmachlearn/pml-training.csv",
               destfile = "train.csv")
@@ -16,7 +18,7 @@ set.seed(69)
 table(tr$classe)/nrow(tr)
 table(tr$new_window)
 table(is.na(tr$max_picth_belt))
-table(tr$new_window)[2]/nrow(tr)
+table(tr$new_window)[1]/nrow(tr)
 ## ONLY FEW YES
 
 x<-tr[tr$new_window=="no",]
@@ -46,11 +48,10 @@ tr<-tr[,-c(1:6)]
 ts<-ts[,-c(1:6)]
 val<-val[,-c(1:6)]
 
-corrplot(cor(tr[,-53]))
 
-cor_mat <- cor(tr[, -53])
-corrplot(cor_mat, order = "FPC", method = "color", type = "upper", 
-         tl.cex = 0.8, tl.col = rgb(0, 0, 0))
+
+corrplot(cor(tr[, -53]), order = "FPC", method = "color", type = "upper", 
+         tl.cex = 0.5, tl.col = rgb(0, 0, 0))
 
 prep<-preProcess(tr,method = c("center","scale"))
 tr<-predict(prep,tr)
@@ -58,20 +59,16 @@ ts<-predict(prep,ts)
 val<-predict(prep,val)
 
 
-pred<-predict(mf,tr)
-confusionMatrix(pred,tr$classe)
-
-
-set.seed(12345)
 ctr<-trainControl(method="repeatedcv",number=5,repeats=5)
-tune<-data.frame(cp=0.0005)
+tune<-data.frame(cp=c(0.1,0.01,0.001,0.0001))
 mf1<-train(classe~.,data=tr,method="rpart",trControl=ctr,tuneGrid=tune)
 
 fancyRpartPlot(mf1$finalModel)
-pre1<-predict(mf1,ts)
 
+pre1<-predict(mf1,ts)
 confusionMatrix(pre1,ts$classe)
 
-pre2<-predict(mf1,val[,-53])
+plot(mf1)
+
+pre2<-predict(mf1,val)
 pre2
-table(pre1,ts$classe)/nrow(ts)
